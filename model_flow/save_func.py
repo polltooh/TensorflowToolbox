5c8b40d4f6606ea8282d3767f240af80d20ad48d
polltooh <polltooh@gmail.com>
2017-02-05 21:29:32 +0800

    add checking dir exists

--- model_flow/save_func.py
+++ model_flow/save_func.py
@@ -37,6 +37,9 @@ def save_model(sess, saver, model_dir, iteration):
     curr_time = time.strftime("%Y%m%d_%H%M")
     model_name = model_dir + '/' + curr_time + \
                             '_iter_' + str(iteration) + '_model.ckpt'
+    if not os.exists(model_dir):
+        os.makedirs(model_dir)
+
     saver.save(sess, model_name)
 
 def add_value_sum(summary_writer, value, name):
