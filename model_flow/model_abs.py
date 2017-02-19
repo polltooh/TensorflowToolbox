8830ad07c9ec5a7fa1f1fe7cee7a19dac6941ab4
polltooh <polltooh@gmail.com>
2017-02-19 14:05:21 +0800

    replace tab

--- model_flow/model_abs.py
+++ model_flow/model_abs.py
@@ -1,17 +1,18 @@
 from abc import ABCMeta, abstractmethod
+import tensorflow as tf
 
 class ModelAbs(object):
-	__metaclass__ = ABCMeta
-	
-	@abstractmethod
-	def model_infer(self, data_ph, model_params):
-		pass
+    __metaclass__ = ABCMeta
 
-	@abstractmethod
-	def model_loss(self, data_ph, model_params):
-		pass
+    @abstractmethod
+    def model_infer(self, data_ph, model_params):
+        raise NotImplementedError
 
-	@abstractmethod
-	def model_mini(self, loss):
-		pass
-	
+    @abstractmethod
+    def model_loss(self, data_ph, model_params):
+        raise NotImplementedError
+
+    @abstractmethod
+    def model_mini(self, loss):
+        raise NotImplementedError
+    
