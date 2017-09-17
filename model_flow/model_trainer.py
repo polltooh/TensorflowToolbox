7671073686bb3ee15a435a7d1084e2f634c7572c
polltooh <polltooh@gmail.com>
2017-09-17 22:06:51 +0200

    ignore no gradient var

--- model_flow/model_trainer.py
+++ model_flow/model_trainer.py
@@ -22,12 +22,16 @@ def average_gradients(grads_list, loss_list):
             #   ((grad0_gpu0, var0_gpu0), ... , (grad0_gpuN, var0_gpuN))
             grads = []
             for g, _ in grad_and_vars:
+                if g is None:
+                    continue
                 # Add 0 dimension to the gradients to represent the tower.
                 expanded_g = tf.expand_dims(g, 0)
                 
                 # Append on a 'tower' dimension which we will average over below.
                 grads.append(expanded_g)
-        
+            
+            if len(grads) == 0:
+                continue
             # Average over the 'tower' dimension.
             grad = tf.concat(grads, 0)
             grad = tf.reduce_mean(grad, 0)
